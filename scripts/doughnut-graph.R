f72e77ae246e9c24a531f9262381e77c26a45fab
Mitchell Isham <>
2020-11-25 20:30:11 -0800

    fixed lint errors

--- scripts/doughnut-graph.R
+++ scripts/doughnut-graph.R
@@ -6,13 +6,16 @@ library(RColorBrewer)
 youtube_trending <- read.csv("data/US_youtube_trending_data.csv",
                              stringsAsFactors = FALSE)
 
+# Changing the column name I will be using later to snake_case
+colnames(youtube_trending)[6] <- "category_id"
+
 # Creating a new data frame that only contains the counts of each Category ID
 count <- youtube_trending %>%
-  group_by(categoryId) %>%
-  summarize(counts = unique((sum(categoryId) / categoryId)))
+  group_by(category_id) %>%
+  summarize(counts = unique((sum(category_id) / category_id)))
 
 # Labeling the Category IDs with their respective categories.
-count$categoryId <- c("Film & Animation", "Autos & Vehicles", "Music",
+count$category_id <- c("Film & Animation", "Autos & Vehicles", "Music",
                      "Pets & Animals", "Sports", "Travel & Events", "Gaming",
                      "People & Blogs", "Comedy", "Entertainment",
                      "News & Politics", "How to & Style", "Education",
@@ -31,7 +34,7 @@ count$ymin <- c(0, head(count$ymax, n = -1))
 count$label_position <- (count$ymax + count$ymin) / 2
 
 # Computing labels
-count$label <- paste0(count$categoryId, "\n", count$counts)
+count$label <- paste0(count$category_id, "\n", count$counts)
 
 # Needed more than 9 colors, so had to concatenate palettes.
 mycolors <- c(brewer.pal(name = "Paired", n = 8),
@@ -40,11 +43,11 @@ mycolors <- c(brewer.pal(name = "Paired", n = 8),
 ### Doughnut graph
 trending_categories_plot <- ggplot(count, aes(ymax = ymax, ymin = ymin,
                                               xmax = 4, xmin = 3,
-                                              fill = categoryId)) +
+                                              fill = category_id)) +
   geom_rect(color = "black") +
   geom_label(x = 4.15,
              aes(y = label_position,
-                 label = paste(categoryId, "\n", percentages, "%")),
+                 label = paste(category_id, "\n", percentages, "%")),
              size = 2.25) +
   scale_color_manual(values = mycolors) +
   coord_polar(theta = "y") +

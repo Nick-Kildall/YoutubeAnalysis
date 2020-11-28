24a53c5b3805d297743369e6ff55d6fe85de9a40
Nick Kildall <nickkildall@Nicks-MacBook-Pro.local>
2020-11-27 18:21:28 -0800

    adding additional content to my graph and starting to add it into the index file

--- scripts/daily_views_graph.R
+++ scripts/daily_views_graph.R
@@ -18,7 +18,7 @@ days_of_week_viewership <- youtube_trending %>%
     "Friday", "Saturday"
   )[as.POSIXlt(substr(publishedAt, 1, 10))$wday + 1]) %>%
   group_by(day) %>%
-  summarise(sum = sum(view_count))
+  summarise(sum_view = sum(view_count))
 
 ### Using factors to put the days of the week in the desired order
 days_of_week_viewership$day <- factor(days_of_week_viewership$day,
@@ -31,9 +31,12 @@ days_of_week_viewership$day <- factor(days_of_week_viewership$day,
 )
 
 ### Creating a bar graph
-days_of_week_viewership_plot <- ggplot(days_of_week_viewership, ) +
-  geom_bar(stat = "identity", mapping = aes(x = day, y = sum, fill = day)) +
+days_of_week_viewership_plot <- ggplot(data = days_of_week_viewership,
+       aes(x = day, y = sum_view, fill = day)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_text(aes(label = sum_view), 
+            position = position_dodge(width = 0.9), vjust = -1) +
   scale_colour_manual(values = names(brewer.pal(6, "Set1"))) +
   labs(x = "Day", y = "Average Views") +
   ggtitle("Average Amount of Views by Day of the Week") +
-  theme(legend.position = "none")
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5))

0df031072007426e46fc135a2e95edf68192871d
Nick Kildall <nickkildall@Nicks-MacBook-Pro.local>
2020-11-27 17:18:16 -0800

    Deleted primary graph because it was too much information. Switched to alternate graph, and modified it to show more useful information

--- scripts/Daily Views Graph.R
+++ scripts/Daily Views Graph.R
@@ -3,17 +3,9 @@
 
 library(dplyr)
 library(ggplot2)
+library(RColorBrewer)
 
-youtube_trending <- read.csv("data/US_youtube_trending_data.csv", stringsAsFactors = FALSE)
-
-daily_viewership <- youtube_trending %>%
-  mutate(date = substr(publishedAt,1,10)) %>%
-  group_by(date) %>%
-  summarise(sum = sum(view_count)) 
-
-daily_viewership_plot <-  ggplot(data = daily_viewership) +
-    geom_col(mapping = aes(x = date, y = sum)) +
-    theme(axis.text.x = element_text(angle = 90))
+youtube_trending <- read.csv("../data/US_youtube_trending_data.csv", stringsAsFactors = FALSE)
 
 ### Alternate graph 
 ### Days of week with most engagement
@@ -23,11 +15,21 @@ days_of_week_viewership <- youtube_trending %>%
                 "Friday", "Saturday"
                 )[as.POSIXlt(substr(publishedAt,1,10))$wday + 1]) %>%
   group_by(day) %>%
-  summarise(sum = sum(view_count) + sum(likes) + sum(dislikes) + sum(comment_count)) 
+  summarise(sum = sum(view_count) ) 
 
-days_of_week_viewership$day <- factor(days_of_week_viewership$day, levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-                                                                              "Friday", "Saturday"))
+days_of_week_viewership$day <- factor(days_of_week_viewership$day, levels =
+                                      c("Sunday", "Monday", "Tuesday",
+                                        "Wednesday", "Thursday", "Friday",
+                                        "Saturday"))
 
-days_of_week_viewership_plot <- ggplot(data = days_of_week_viewership) +
-  geom_col(mapping = aes(x = day, y = sum)) 
+days_of_week_viewership_plot <- ggplot(days_of_week_viewership, ) +
+  geom_bar(stat="identity", mapping = aes(x = day, y = sum, fill = day)) +
+  scale_colour_manual(values = names(brewer.pal(6, "Set1"))) +
+  labs(x = "Day", y = "Average Views") +
+  ggtitle("Average Amount of Views by Day of the Week") +
+  theme(legend.position="none") 
+
+
+
+
 

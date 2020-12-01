8b7370bbcbdaf0a51fbfcb3c3736c703dd2ff8b4
Isaac Afolayan <timie@Isaacs-MacBook-Pro.local>
2020-11-30 17:20:44 -0800

    finished aggragate table script

--- scripts/Grouped_by.R
+++ scripts/Grouped_by.R
@@ -6,48 +6,51 @@ library(dplyr)
 library(leaflet)
 library(ggplot2)
 library(stringr)
-youtube <- read.csv("data/US_youtube_trending_data.csv", stringsAsFactors = FALSE)
 
+youtube <- read.csv("data/US_youtube_trending_data.csv", stringsAsFactors = 
+                    FALSE)
 
 get_table_info <- function(youtube) {
-
   views_mean <- youtube%>%
     group_by(categoryId)%>%
     summarise(averageviews = mean(view_count))%>%
     pull(averageviews)
+  
   averagelikes <- youtube%>%
     group_by(categoryId)%>%
     summarise(averagelikes = mean(likes))%>%
     pull(averagelikes)
+  
   topchannel <- youtube%>%
     select(channelTitle, view_count, categoryId)%>%
     group_by(categoryId)%>%
     filter(view_count == max(view_count))%>%
-    arrange(categoryId)
+    arrange(categoryId)%>%
     pull(channelTitle)
+  
   commentsmean <- youtube%>%
     select(comment_count, categoryId)%>%
     group_by(categoryId)%>%
     summarise(comment_count = mean(comment_count))%>%
-    arrange(categoryId)
+    arrange(categoryId)%>%
     pull(comment_count)
+  
   titlestat <- youtube%>%
     mutate(per_cap = str_count(youtube$title, "[A-Z]") / nchar(youtube$title))%>%
     group_by(categoryId)%>%
     summarise(avg_per_cap = paste0(round(100 * mean(per_cap), 1), "%"))%>%
     pull(avg_per_cap)
-  data.frame(views_mean, averagelikes, topchannel, commentsmean, titlestat)
-  return ()
+  
+  category_names <- c("Film & Animation", "Autos & Vehicles", "Music",
+                   "Pets & Animals", "Sports", "Travel & Events", "Gaming",
+                   "People & Blogs", "Comedy", "Entertainment",
+                   "News & Politics", "How to & Style", "Education",
+                   "Science & Technology", "Nonprofits & Activism")
+  aggregate <- data.frame(category_names, views_mean, averagelikes, topchannel,
+                          commentsmean, titlestat)
+  return (aggregate)
 }
 
-data.frame
-
-
-str_count(youtube$title, "[A-Z]") / nchar(youtube$title)
-Average views
-Average like / dislike ratio
-Top channel per category
-Average number of comments 
-Title statistics (average percent of letters that are capitalized)
+aggregate_table <- get_table_info(youtube)
 
 

72a21f8b1f427f9ac75cb19e63be1eacd08d3cff
Nick Kildall <nickkildall@Nicks-MacBook-Pro.local>
2020-12-07 16:50:25 -0800

    removed excess code

--- app_ui.R
+++ app_ui.R
@@ -7,16 +7,6 @@ library("dplyr")
 library("shinythemes")
 library("shinydashboard")
 
-### Nick
-
-### Mitchell
-
-### Quang
-
-### Isaac
-
-
-
 header <- dashboardHeader(title = "YouTube Trending Data Analysis")
 
 sidebar <- dashboardSidebar(
@@ -74,35 +64,3 @@ ui <- dashboardPage(
   sidebar,
   body
 )
-
-#ui_final <- navbarPage(
-#  "Midwest Dataset",
-#  tabPanel("page one", page_one),
-#  tabPanel("page two", page_two)
-#)
-
-#### page one
-#page_one <- fluidPage(
-#  sidebarPanel(
-#    y_input1,
-#    color_input
-#  ),
-#  mainPanel(
-#    h2("Midwest Plot"),
-#    textOutput(outputId = "msg"),
-#    plotOutput(outputId = "midwest_one")
-#  )
-#)
-
-#### page two
-#page_two <- fluidPage(
-#  sidebarPanel(
-#    y_input2,
-#    state_input
-#  ),
-#  mainPanel(
-#    h2("Midwest Plot"),
-#    textOutput(outputId = "msg2"),
-#    plotOutput(outputId = "midwest_two")
-#  )
-#)
